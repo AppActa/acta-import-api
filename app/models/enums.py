c91533ffe6cc6bbb34af005a65d1961226da9f2c
Davi Aliaga <kavhsao@gmail.com>
2026-09-21 01:54:31 -0300

    feat/enums: adiciona novas categorias e status como enums, e define extensões permitidas para tipos de imagem e arquivo

--- app/models/enums.py
+++ app/models/enums.py
@@ -1,22 +1,5 @@
 from enum import Enum
 
-class TipoImagem(Enum):
-    JPEG = 'image/jpeg'
-    PNG = 'image/png'
-    WEBP = 'image/webp'
-    GIF = 'image/gif'
-    SVG = 'image/svg+xml'
-    BMP = 'image/bmp'
-    TIFF = 'image/tiff'
-
-class TipoArquivo(Enum):
-    PDF = 'application/pdf'
-    DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-    PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-    XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    CSV = 'text/csv'
-    TXT = 'text/plain'
-
 class Categoria(Enum):
     TREINAMENTO = 'TREINAMENTO'
     PLANO_ACAO = 'PLANO_ACAO'
@@ -34,3 +17,34 @@ class Status(Enum):
     ATIVO = 'ATIVO'
     ERRO = 'ERRO'
     EXCLUIDO = 'EXCLUIDO'
+
+class TipoImagem(Enum):
+    JPEG = 'image/jpeg'
+    PNG = 'image/png'
+    WEBP = 'image/webp'
+    GIF = 'image/gif'
+    SVG = 'image/svg+xml'
+    BMP = 'image/bmp'
+    TIFF = 'image/tiff'
+
+class TipoArquivo(Enum):
+    PDF = 'application/pdf'
+    DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    CSV = 'text/csv'
+    TXT = 'text/plain'
+
+# não incluso svg, txt e csv porque são texto puro e devem ser validados de outra forma
+EXTENSOES_PERMITIDAS = set({
+    TipoImagem.JPEG: 'jpg',
+    TipoImagem.PNG: 'png',
+    TipoImagem.WEBP: 'webp',
+    TipoImagem.GIF: 'gif',
+    TipoImagem.BMP: 'bmp',
+    TipoImagem.TIFF: 'tif',
+    TipoArquivo.PDF: 'pdf',
+    TipoArquivo.DOCX: 'docx',
+    TipoArquivo.PPTX: 'pptx',
+    TipoArquivo.XLSX: 'xlsx'
+}.values())
