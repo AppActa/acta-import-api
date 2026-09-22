060650b3cadf27938990aede298b78029293c777
Davi Aliaga <kavhsao@gmail.com>
2026-09-22 19:39:08 -0300

    feat/api: adiciona tratamento de erros personalizados na API e atualiza README com informações detalhadas sobre a funcionalidade de importação de anexos

--- worker.py
+++ worker.py
@@ -1,9 +1,9 @@
 from redis import Redis
-from rq import Worker
+from rq import SpawnWorker
 
 from app.database.redis import get_pool
 from app.services.fila import fila_uploads
 
 
 if __name__ == '__main__':
-    Worker([fila_uploads], connection=Redis(connection_pool=get_pool())).work()
+    SpawnWorker([fila_uploads], connection=Redis(connection_pool=get_pool())).work()
